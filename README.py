a065818b7a61cea029491e7e6038203dbb3bcefb
KiraVelieva-CH <Kiravelieva@icloud.com>
2026-06-02 13:22:36 +0000

    README.py

--- README.py
+++ README.py
@@ -1,3 +1,2 @@
-#Siteomix is a PyMOL plugin designed to calculate binding sites in the form of point clouds 
-# and subsequently identify the structural similarity of binding sites by aligning the clouds and calculating the total overlap.
+#Siteomix is a PyMOL plugin designed to calculate binding sites in the form of point clouds and subsequently identify the structural similarity of binding sites by aligning the clouds and calculating the total overlap.
 
