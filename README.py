eff9e6b09458bb244003a93b916820132f6f4a85
Kira M. Velieva <Kiravelieva@icloud.com>
2026-07-27 11:14:08 +0000

    README.py

--- README.py
+++ README.py
@@ -1,2 +1,4 @@
 #Siteomix is a PyMOL plugin designed to calculate binding sites in the form of point clouds and subsequently identify the structural similarity of binding sites by aligning the clouds and calculating the total overlap.
-# The DOI of the scientific article that the plugin was developed from will be added soon
+#The DOI of the scientific article that the plugin was developed from will be added soon
+#The "Examples of binding site studies using Siteomix - KRAS, p53, kinases, GPCRs" folder contains other examples of screening the molecules indicated in the titles. 
+#Attached are PDF files with the molecules and calculated binding sites, screenshots of the plugin log after calculating the similarity coefficient, and a document with all the results.
